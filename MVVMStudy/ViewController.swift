fb318b0e16ba2b0fe076c5813b879220f18eae7a
Mert TUNÇBİLEK <mert.tuncbilek@evrenselsoft.com>
2019-12-12 13:57:36 +0300

    Initial Commit

---
+++ MVVMStudy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMStudy
+//
+//  Created by Mert TUNÇBİLEK on 12.12.2019.
+//  Copyright © 2019 Evrensel Software Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
